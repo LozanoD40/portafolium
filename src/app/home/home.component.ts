4a4b1ec6e4d9801d37c331775627b8bba866dcca
Lozano Mora Diego <lozano.mora.diego@gmail.com>
2025-09-06 10:20:19 -0500

    Eliminacion de archivos innecesarios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from '@auth0/auth0-angular'; // Para usar Auth0
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrls: ['./home.css']
+  //styleUrls: ['./assets/styles.css']
 })
 export class HomeComponent implements OnInit, AfterViewInit {
   constructor(public auth: AuthService) { }
